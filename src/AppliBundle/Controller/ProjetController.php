0f0827578a9a5675a3415ca9fc9967d4ed5dc99a
Mac Mini <macmini@Mac-mini-de-Mac.local>
2017-01-03 12:59:43 +0100

    commented

--- src/AppliBundle/Controller/ProjetController.php
+++ src/AppliBundle/Controller/ProjetController.php
@@ -26,7 +26,10 @@ class ProjetController extends Controller
      */
     public function indexAction(User $user)
     {
+        // Controle de l'utilisateur
         if ($user === $this->getUser()){
+
+            //Récupération des données du projet et données liées
             $em = $this->getDoctrine()->getManager();
 
             $projets = $em->getRepository('AppliBundle:Projet')->findByUtilisateur($user);
@@ -40,13 +43,15 @@ class ProjetController extends Controller
 
             ));
         }
+
+        // Si mauvais Utilisateur on renvoie vers page Unauthorized
         else{
             return $this->render('AppliBundle:Default:unauthorized.html.twig');
         }
     }
 
     /**
-     * Creates a new projet entity.
+     * Creates a new projet entity with user ID
      *
      * @Route("/{id}/new", name="projet_new")
      *
@@ -54,57 +59,68 @@ class ProjetController extends Controller
      */
     public function newAction(Request $request, User $user)
     {
+        // Controle de l'utilisateur
         if ($user === $this->getUser()){
+
+            // Création du nouveau projet
             $projet = new Projet();
-        $projet->setUtilisateur($user);
-        $form = $this->createForm('AppliBundle\Form\ProjetType', $projet);
-        $form->handleRequest($request);
 
+            // Attribution de l'utilisateur au projet
+            $projet->setUtilisateur($user);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($projet);
-            $em->flush($projet);
+            // Formulaire de création du projet
+            $form = $this->createForm('AppliBundle\Form\ProjetType', $projet);
+            $form->handleRequest($request);
 
-            return $this->redirectToRoute('script_index', array(
-                'id' => $user->getId(),
-                'projet' => $projet->getId(),
+            // Si Formulaire valide, on persiste dans la BD
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($projet);
+                $em->flush($projet);
+
+                // et on renvoie vers la création du script
+                return $this->redirectToRoute('script_index', array(
+                    'id' => $user->getId(),
+                    'projet' => $projet->getId(),
+                ));
+            }
+
+            // Si formulaire invalide, on recommence
+            return $this->render('projet/new.html.twig', array(
+                'user' => $user,
+                'projet' => $projet,
+                'form' => $form->createView(),
             ));
-        }
+            }
 
-        return $this->render('projet/new.html.twig', array(
-            'user' => $user,
-            'projet' => $projet,
-            'form' => $form->createView(),
-        ));
-        }
+        // Si mauvais Utilisateur on renvoie vers page Unauthorized
         else{
             return $this->render('AppliBundle:Default:unauthorized.html.twig');
         }
     }
-
-    /**
-     * Finds and displays a projet entity.
-     *
-     * @Route("/{id}/{projet}", name="projet_show")
-     * @Method("GET")
-     */
-    public function showAction(User $user, Projet $projet)
-    {
-        if ($user === $this->getUser()){
-
-        $deleteForm = $this->createDeleteForm($projet, $user);
-
-        return $this->render('projet/show.html.twig', array(
-            'user' => $user,
-            'projet' => $projet,
-            'delete_form' => $deleteForm->createView(),
-        ));
-        }
-        else{
-            return $this->render('AppliBundle:Default:unauthorized.html.twig');
-        }
-    }
+//
+//    /**
+//     * Finds and displays a projet entity.
+//     *
+//     * @Route("/{id}/{projet}", name="projet_show")
+//     * @Method("GET")
+//     */
+//    public function showAction(User $user, Projet $projet)
+//    {
+//        if ($user === $this->getUser()){
+//
+//        $deleteForm = $this->createDeleteForm($projet, $user);
+//
+//        return $this->render('projet/show.html.twig', array(
+//            'user' => $user,
+//            'projet' => $projet,
+//            'delete_form' => $deleteForm->createView(),
+//        ));
+//        }
+//        else{
+//            return $this->render('AppliBundle:Default:unauthorized.html.twig');
+//        }
+//    }
 
     /**
      * Displays a form to edit an existing projet entity.
@@ -114,24 +130,32 @@ class ProjetController extends Controller
      */
     public function editAction(User $user, Request $request, Projet $projet)
     {
+
+        // Controle de l'utilisateur
         if ($user === $this->getUser()){
-        $deleteForm = $this->createDeleteForm($projet, $user);
-        $editForm = $this->createForm('AppliBundle\Form\ProjetType', $projet);
-        $editForm->handleRequest($request);
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('projet_index', array('id' => $user->getId()));
+//        $deleteForm = $this->createDeleteForm($projet, $user);
+            // création du formulaire avec données remplies
+            $editForm = $this->createForm('AppliBundle\Form\ProjetType', $projet);
+            $editForm->handleRequest($request);
+
+            // si formulaire valide on modifie les données dans la BDD
+            if ($editForm->isSubmitted() && $editForm->isValid()) {
+                $this->getDoctrine()->getManager()->flush();
+
+                // et on redirige vers la liste des projets
+                return $this->redirectToRoute('projet_index', array('id' => $user->getId()));
         }
-
-        return $this->render('projet/edit.html.twig', array(
-            'user' => $user,
-            'projet' => $projet,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+            // sinon formulaire non valide on reessaie
+            return $this->render('projet/edit.html.twig', array(
+                'user' => $user,
+                'projet' => $projet,
+                'edit_form' => $editForm->createView(),
+//                'delete_form' => $deleteForm->createView(),
         ));
         }
+        // Si mauvais Utilisateur on renvoie vers page Unauthorized
         else{
             return $this->render('AppliBundle:Default:unauthorized.html.twig');
         }
@@ -145,17 +169,22 @@ class ProjetController extends Controller
      */
     public function deleteAction(User $user, Request $request, Projet $projet)
     {
-//        $form = $this->createDeleteForm($projet, $user);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
+        // Controle de l'utilisateur
+        if ($user === $this->getUser()){
+
+            // Suppression dans la BDD du projet séléctionné
             $em = $this->getDoctrine()->getManager();
             $em->remove($projet);
             $em->flush($projet);
-//        }
-
-        return $this->redirectToRoute('projet_index', array('id' => $user->getId()));
+            // et on redirige vers la liste des projets
+            return $this->redirectToRoute('projet_index', array('id' => $user->getId()));
+        }
+        // Si mauvais Utilisateur on renvoie vers page Unauthorized
+        else{
+            return $this->render('AppliBundle:Default:unauthorized.html.twig');
+        }
     }
+
 
     /**
      * Creates a form to delete a projet entity.
--- src/AppliBundle/Controller/ScriptController.php
+++ src/AppliBundle/Controller/ScriptController.php
@@ -23,6 +23,7 @@ class ScriptController extends Controller
 {
 
     /**
+     * From Projet_index - Button "Ecrire le script"
      * Creates a new script entity.
      *
      * @Route("/user={id}/projet={projet}/questions", name="script_index")
@@ -30,35 +31,36 @@ class ScriptController extends Controller
      */
     public function newAction(Request $request, User $user, Projet $projet)
     {
+        // Controle de l'utilisateur
         if ($user === $this->getUser()){
-            $script = new Script();
-            $script->setProjet($projet->getId());
 
+            // Création d'unee l'entité Script
+            $script = new Script();
+
+            // Attribution de l'Id du Projet au script
+            $script->setProjet($projet->getId());
 
             $form = $this->createForm('AppliBundle\Form\ScriptType', $script);
             $form->handleRequest($request);
-
             $em = $this->getDoctrine()->getManager();
             $em->persist($script);
             $em->flush($script);
 
-
+            // Attribution de l'Id du script au Projet
             $projet->setScript($script->getId());
             $em2 = $this->getDoctrine()->getManager();
             $em2->persist($projet);
             $em2->flush($projet);
 
-
-
+            // Redirection vers les questions de base du script
             return $this->redirectToRoute('script_questions', array(
                 'script' => $script->getId(),
                 'id' => $user->getId(),
                 'projet' => $projet->getId(),
             ));
+   }
 
-
-
-        }
+        // Si mauvais Utilisateur on renvoie vers page Unauthorized
         else{
             return $this->render('AppliBundle:Default:unauthorized.html.twig');
         }
@@ -67,6 +69,8 @@ class ScriptController extends Controller
 
 
     /**
+     * From Projet_index - Button "Accéder au script"
+     * Or from Script newAction
      * Ask base questions.
      *
      * @Route("/questions/user={id}/projet={projet}", name="script_questions")
@@ -74,183 +78,218 @@ class ScriptController extends Controller
      */
     public function questionsAction(Request $request, User $user, Projet $projet)
     {
-        // On recherche toutes les questions à poser
-        $em = $this->getDoctrine()->getManager();
-        $questions = $em->getRepository('AppliBundle:ScriptQuestion')->findAll();
+        // Controle de l'utilisateur
+        if ($user === $this->getUser()){
+
+            // On recherche toutes les questions à poser
+            $em = $this->getDoctrine()->getManager();
+            $questions = $em->getRepository('AppliBundle:ScriptQuestion')->findAll();
 
 
-        // On vérifie s'il n'y a pas déjà des réponses pour ce script
-        $repository = $this->getDoctrine()
-            ->getManager()
-            ->getRepository('AppliBundle:ScriptReponse');
-        $reponses = $repository->findByScript($projet->getScript());
+            // On vérifie s'il n'y a pas déjà des réponses pour ce script
+            $repository = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('AppliBundle:ScriptReponse');
+            $reponses = $repository->findByScript($projet->getScript());
 
-        if ($reponses){
-            return $this->render('script/edit.html.twig', array(
+            // Si on a des réponses existantes pour le scritp on renvoie vers formulaire Script Edit
+            if ($reponses){
+                return $this->render('script/edit.html.twig', array(
+                    'user' => $user->getId(),
+                    'projet' => $projet,
+                    'questions' => $questions,
+                    'reponses' => $reponses,
 
-                'user' => $user->getId(),
-                'projet' => $projet,
-                'questions' => $questions,
-                'reponses' => $reponses,
-
-            ));
+                ));
+            }
+            // Sinon on renvoie vers formulaire Script New
+            else{
+                return $this->render('script/new.html.twig', array(
+                    'user' => $user->getId(),
+                    'projet' => $projet,
+                    'questions' => $questions,
+                    ));
+            }
         }
-        // On envoie à la page script_index, formulaire des questions de base
+
+        // Si mauvais Utilisateur on renvoie vers page Unauthorized
         else{
-            return $this->render('script/index.html.twig', array(
-
-                'user' => $user->getId(),
-                'projet' => $projet,
-                'questions' => $questions,
-
-
-            ));
+            return $this->render('AppliBundle:Default:unauthorized.html.twig');
         }
 
     }
 
 
     /**
-     * Flush answers to questions in DB.
+     * From questionsAction si pas de réponses existantes
      *
      * @Route("/reponsesnew/user={id}/projet={projet}/script={script}", name="script_reponses_new")
      * @Method({"GET", "POST"})
      */
     public function reponsesNewAction(Request $request, User $user, Projet $projet, Script $script)
     {
+        // Controle de l'utilisateur
+        if ($user === $this->getUser()){
 
-        foreach ($_POST as $key=>$reponse){
+            // Pour chaque question/reponse
+            foreach ($_POST as $key=>$reponse){
 
-            $questionId = str_replace("reponse", "", $key);
+                // On récupère l'id de la question
+                $questionId = $key;
 
-            $repository = $this->getDoctrine()
-                ->getManager()
-                ->getRepository('AppliBundle:ScriptQuestion');
+                // Et on récupère l'entité question correspondante
+                $repository = $this->getDoctrine()
+                    ->getManager()
+                    ->getRepository('AppliBundle:ScriptQuestion');
+                $question = $repository->findOneById($questionId);
 
-            $question = $repository->findOneById($questionId);
+                // Création de la nouvelle entité script réponse
+                $scriptReponse = new ScriptReponse();
 
-            $scriptReponse = new ScriptReponse();
-            $scriptReponse->setScript($script->getId());
-            $scriptReponse->setQuestion($question);
-            $scriptReponse->setReponse($reponse);
+                // Attribution du script, de la question et de la réponse
+                $scriptReponse->setScript($script->getId());
+                $scriptReponse->setQuestion($question);
+                $scriptReponse->setReponse($reponse);
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($scriptReponse);
-            $em->flush($script);
+                // Envoi dans la BDD
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($scriptReponse);
+                $em->flush($script);
+
+            }
+            // Redirection vers l'écriture du script
+            return $this->redirectToRoute('scriptecriture_new', array(
+                'script' => $script->getId(),
+                'id' => $user->getId(),
+                'projet' => $projet->getId(),
+            ));
 
         }
-        return $this->redirectToRoute('scriptecriture_new', array(
-            'script' => $script->getId(),
-            'id' => $user->getId(),
-            'projet' => $projet->getId(),
-        ));
+
+        // Si mauvais Utilisateur on renvoie vers page Unauthorized
+        else{
+            return $this->render('AppliBundle:Default:unauthorized.html.twig');
+        }
     }
 
 
 
     /**
-     * Flush modificated answers to questions in DB.
+     * From questionsAction si déjà des réponses existantes
      *
      * @Route("/reponsesedit/user={id}/projet={projet}/script={script}", name="script_reponses_edit")
      * @Method({"GET", "POST"})
      */
     public function reponsesEditAction(Request $request, User $user, Projet $projet, Script $script)
     {
+        // Controle de l'utilisateur
+        if ($user === $this->getUser()){
 
+            // Pour chaque question/reponse
+            foreach ($_POST as $key=>$reponse){
 
-        foreach ($_POST as $key=>$reponse){
+                //On récupère l'entité ScriptReponse correspondante
+                $scriptReponse = $this->getDoctrine()->getRepository('AppliBundle:ScriptReponse')->findOneById($key);
 
-            $scriptReponse = $this->getDoctrine()->getRepository('AppliBundle:ScriptReponse')->findOneById($key);
-            $scriptReponse->setReponse($reponse);
-            $this->getDoctrine()->getEntityManager()->flush();
+                // Et on modifie la réponse dans la BDD
+                $scriptReponse->setReponse($reponse);
+                $this->getDoctrine()->getEntityManager()->flush();
+
+            }
+            // Redirection vers l'écriture du script
+            return $this->redirectToRoute('scriptecriture_new', array(
+                'script' => $script->getId(),
+                'id' => $user->getId(),
+                'projet' => $projet->getId(),
+            ));
 
         }
-        return $this->redirectToRoute('scriptecriture_new', array(
-            'script' => $script->getId(),
-            'id' => $user->getId(),
-            'projet' => $projet->getId(),
-        ));
-    }
 
-
-
-
-
-
-    /**
-     * Finds and displays a script entity.
-     *
-     * @Route("/user={id}", name="script_show")
-     * @Method("GET")
-     */
-    public function showAction(Script $script)
-    {
-        $deleteForm = $this->createDeleteForm($script);
-
-        return $this->render('script/show.html.twig', array(
-            'script' => $script,
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Displays a form to edit an existing script entity.
-     *
-     * @Route("/{id}/edit", name="script_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Script $script)
-    {
-        $deleteForm = $this->createDeleteForm($script);
-        $editForm = $this->createForm('AppliBundle\Form\ScriptType', $script);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('script_edit', array('id' => $script->getId()));
+        // Si mauvais Utilisateur on renvoie vers page Unauthorized
+        else{
+            return $this->render('AppliBundle:Default:unauthorized.html.twig');
         }
-
-        return $this->render('script/edit.html.twig', array(
-            'script' => $script,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
     }
 
-    /**
-     * Deletes a script entity.
-     *
-     * @Route("/{id}", name="script_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Script $script)
-    {
-        $form = $this->createDeleteForm($script);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($script);
-            $em->flush($script);
-        }
 
-        return $this->redirectToRoute('script_index');
-    }
-
-    /**
-     * Creates a form to delete a script entity.
-     *
-     * @param Script $script The script entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Script $script)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('script_delete', array('id' => $script->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
+//
+//
+//
+//    /**
+//     * Finds and displays a script entity.
+//     *
+//     * @Route("/user={id}", name="script_show")
+//     * @Method("GET")
+//     */
+//    public function showAction(Script $script)
+//    {
+//        $deleteForm = $this->createDeleteForm($script);
+//
+//        return $this->render('script/show.html.twig', array(
+//            'script' => $script,
+//            'delete_form' => $deleteForm->createView(),
+//        ));
+//    }
+//
+//    /**
+//     * Displays a form to edit an existing script entity.
+//     *
+//     * @Route("/{id}/edit", name="script_edit")
+//     * @Method({"GET", "POST"})
+//     */
+//    public function editAction(Request $request, Script $script)
+//    {
+//        $deleteForm = $this->createDeleteForm($script);
+//        $editForm = $this->createForm('AppliBundle\Form\ScriptType', $script);
+//        $editForm->handleRequest($request);
+//
+//        if ($editForm->isSubmitted() && $editForm->isValid()) {
+//            $this->getDoctrine()->getManager()->flush();
+//
+//            return $this->redirectToRoute('script_edit', array('id' => $script->getId()));
+//        }
+//
+//        return $this->render('script/edit.html.twig', array(
+//            'script' => $script,
+//            'edit_form' => $editForm->createView(),
+//            'delete_form' => $deleteForm->createView(),
+//        ));
+//    }
+//
+//    /**
+//     * Deletes a script entity.
+//     *
+//     * @Route("/{id}", name="script_delete")
+//     * @Method("DELETE")
+//     */
+//    public function deleteAction(Request $request, Script $script)
+//    {
+//        $form = $this->createDeleteForm($script);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $em = $this->getDoctrine()->getManager();
+//            $em->remove($script);
+//            $em->flush($script);
+//        }
+//
+//        return $this->redirectToRoute('script_index');
+//    }
+//
+//    /**
+//     * Creates a form to delete a script entity.
+//     *
+//     * @param Script $script The script entity
+//     *
+//     * @return \Symfony\Component\Form\Form The form
+//     */
+//    private function createDeleteForm(Script $script)
+//    {
+//        return $this->createFormBuilder()
+//            ->setAction($this->generateUrl('script_delete', array('id' => $script->getId())))
+//            ->setMethod('DELETE')
+//            ->getForm()
+//        ;
+//    }
 }
